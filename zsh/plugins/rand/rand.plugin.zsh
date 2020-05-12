4e6c9e3db3c1f47ad9ec206de08d9c563bfa6c1d
Maxim Puchkov <maxim_puchkov@sfu.ca>
2020-05-11 21:44:00 -0700

    [CS866-0511] 1 file changed, 16 insertions(+).
    Signed-off-by: Maxim Puchkov <maxim_puchkov@sfu.ca>

--- zsh/plugins/rand/rand.plugin.zsh
+++ zsh/plugins/rand/rand.plugin.zsh
@@ -1,6 +1,22 @@
 
 
 
+function irand() {
+    integer max=$1
+    integer rand=$(( $RANDOM % $max ))
+    printf '%d' $rand
+}
+
+function airand() {
+    integer max=$1
+    integer count=$2
+    typeset -ag rand=()
+    
+    integer i=1
+    for i in {1..$count}; do
+        rand+=$(irand $max)
+    done
+}
 
 function randstr() {
 
