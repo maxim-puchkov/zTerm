32fbc1b1013a7d42393c2354041d120036d6eb9c
Maxim Puchkov <maxim_puchkov@sfu.ca>
2020-06-12 19:25:54 -0700

    Z-Shell functions update

--- zsh/plugins/note/note.plugin.zsh
+++ zsh/plugins/note/note.plugin.zsh
@@ -4,8 +4,13 @@
 
 
 function note() {
+    typeset name=$1
+    typeset -ag text=("${(@f)argv}")
     mkdir -p ~/etc
     touch ~/etc/note
-    
+#    typeset -a line
+#    read -t -u0 -A line
+    typeset prog="%D{%c} %F{11}%B${0}%b ${1}%f"
+    logf $prog
 }
 
